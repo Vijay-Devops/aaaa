ab4f735c5c528e0cd2299a67a6d312fd505aff93
sriniwaz <niwaz841@gmail.com>
2019-09-24 12:16:05 +0530

    Update Demo.java

--- src/aaaa/Demo.java
+++ src/aaaa/Demo.java
@@ -15,8 +15,7 @@ public class Demo {
 		System.out.println("hello vijay");
 		System.out.println("hey sankar");
 		System.out.println("hey you");
-		
-		
+		System.out.println("if you are bad im your dad");
 
 		
 	}
